8408a17abcb02ff38304428bfda29baacfd9ee1a
Thisseas X <thisseasx@yahoo.com>
2019-12-24 20:13:24 +0200

    Add allowedMethods

--- src/index.js
+++ src/index.js
@@ -6,10 +6,11 @@ import { pretty, cors, welcome } from './middleware';
 const app = new Koa();
 
 app
+  .use(cors())
   .use(bodyParser())
   .use(rootController.routes())
+  .use(rootController.allowedMethods())
   .use(pretty())
-  .use(cors())
   .use(welcome())
   .listen(process.env.PORT || 3000, function() {
     console.log(`Server listening on port: ${this.address().port}`);
